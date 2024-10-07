2278e5cd1d8b8c6dd5f4971e5d4b2042011bed6c
KshitizDayal <kshitizdayal14@gmail.com>
2024-10-07 10:30:56 +0530

    colors added

---
+++ packages/fodzsy_ds/lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color primary = Color(0xFFFF885B);
+  static const Color secondary = Color(0xFFFFE5CF);
+  static const Color tertiary = Color(0xFF973131);
+  static const Color quaternary = Color(0xFFE0A75E);
+
+  static const Color green = Color(0xFF557C56);
+  static const Color red = Color(0xFFFF0000);
+}
--- packages/fodzsy_ds/lib/fodzsy_ds.dart
+++ packages/fodzsy_ds/lib/fodzsy_ds.dart
@@ -1,7 +1,1 @@
 library fodzsy_ds;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
